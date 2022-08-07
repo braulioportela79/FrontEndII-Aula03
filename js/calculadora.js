18bd404fe7553aa04085b9ea6fccad0ac0dcab45
braulioportela79 <braulioportela@outlook.com>
2022-08-07 13:44:54 -0300

    Exercício Finalizado

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,8 +3,6 @@ import subtrair from './subtrair.js';
 import multiplicar from './multiplicar.js';
 import dividir from './dividir.js';
 
-
-
 document.querySelector('#somar').addEventListener('click', somar);
 document.querySelector('#subtrair').addEventListener('click', subtrair);
 document.querySelector('#multiplicar').addEventListener('click', multiplicar);
