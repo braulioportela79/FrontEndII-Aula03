aa93c6fb5c14f3ca1dff41202996e7ae444dfc74
braulioportela79 <braulioportela@outlook.com>
2022-08-06 23:14:24 -0300

    Exercício Finalizado

--- js/multiplicar.js
+++ js/multiplicar.js
@@ -1,14 +1,8 @@
 // 3. Criar um arquivo multiplicar.js contendo uma função chamada multiplicar, que deve ser exportada no final do arquivo. Esta função deve receber 2 parâmetros e retornar a multiplicação dos mesmos. Importante:
 // a. Contemplar o cenário onde se um dos dois parâmetros for zero, a função retornará zero.
 
-function multiplicar(num1, num2) {
-    if (num1 == 0 | num2 == 0) { return 0 };
+const multiplicar = (num1, num2) => {
     return num1 * num2;
 };
-
-// const multiplicar = (num1, num2) => {
-//     if (num1 == 0 | num2 == 0) { return 0 }
-//     return num1 * num2;
-// };
 
 export default multiplicar;
